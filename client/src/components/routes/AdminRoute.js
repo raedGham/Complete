e52fd8f08b926646e1dfc9146e5a9d4f19d31127
Raed Ghamrawi <raed_ghamrawi@live.com>
2021-12-09 19:37:53 +0200

    corrected admin and user routes

--- client/src/components/routes/AdminRoute.js
+++ client/src/components/routes/AdminRoute.js
@@ -1,38 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import { Navigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import {currentAdmin} from "../../functions/auth";
+import { currentAdmin } from "../../functions/auth";
+import LoadingToRedirect from './LoadingToRedirect';
 
-// this module is not working properly, it should implement protected routes but cannot access state!!!
 
-const AdminRoute = ({ children }) => { 
-    
-    const  {user}  = useSelector((state) => ({...state}));
-    
+const AdminRoute = ({ children }) => {
+
+    const { user } = useSelector((state) => ({ ...state }));
+
 
     const [ok, setOk] = useState(false);
-     console.log("Admin Route user", user);
-    useEffect(()=> {
-        
-        if(user && user.token) {
+    console.log("Admin Route user", user);
+    useEffect(() => {
+
+        if (user && user.token) {
             currentAdmin(user.token)
-            .then( (res) => {
-                console.log("Current ADMIN response", res)
-                setOk(true);                
-            })
-            .catch((err) => {
-                console.log(" ADMIN route error", err);
-                setOk(false);
-                
-            })
-            
+                .then((res) => {
+                    console.log("Current ADMIN response", res)
+                    setOk(true);
+                })
+                .catch((err) => {
+                    console.log(" ADMIN route error", err);
+                    setOk(false);
+
+                })
+
         }
-         
+
     }, [user]);
-  
 
-    return   ok ? children : <Navigate to="/login"/>;
 
+    return ok ? children : <LoadingToRedirect />;
+    //<Navigate to="/login"/>
 
 }
 
--- client/src/components/routes/UserRoute.js
+++ client/src/components/routes/UserRoute.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Navigate} from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
+import { useSelector } from 'react-redux';
+import LoadingToRedirect from './LoadingToRedirect';
 
 // this module is not working properly, it should implement protected routes but cannot access state!!!
 
-const UserRoute = ({ children }) => { 
-    const  {user}  = useSelector((state) => ({...state}));
- console.log(user);
-   // return user && user.token ? children : <Navigate to="/login"/>;
-
-    return   true ? children : <Navigate to="/login"/>;
+const UserRoute = ({ children }) => {
+  const { user } = useSelector((state) => ({ ...state }));
+  console.log(user);
+  return user && user.token ? children : <LoadingToRedirect />;
 
 
 }
